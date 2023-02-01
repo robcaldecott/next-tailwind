43be1df958ea3e3464dbc65cf0ca805abab6bf05
Rob Caldecott <robert.caldecott@gmail.com>
2023-02-01 10:38:01 +0000

    Syntax highlighter example

--- pages/syntax2.tsx
+++ pages/syntax2.tsx
@@ -1,7 +1,7 @@
 import Syntax from "react-syntax-highlighter";
 
 export default function Page() {
-  const codeString = "(num) => num + 1";
+  const codeString = "function add(x: number, y: number) {\n  return x + y;\n}";
   return (
     <div className="border-2 border-slate-600">
       <Syntax language="javascript">{codeString}</Syntax>
