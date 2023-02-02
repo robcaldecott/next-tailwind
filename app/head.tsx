ac468e623168a73ffab9419e3d264e08c974b630
Rob Caldecott <robert.caldecott@gmail.com>
2023-02-02 10:33:08 +0000

    tweak

--- app/head.tsx
+++ app/head.tsx
@@ -3,6 +3,7 @@ export default function Head() {
     <>
       <title>Vehicle Manager</title>
       <link rel="icon" href="/favicon.ico" />
+      <meta name="viewport" content="initial-scale=1, width=device-width" />
     </>
   );
 }
